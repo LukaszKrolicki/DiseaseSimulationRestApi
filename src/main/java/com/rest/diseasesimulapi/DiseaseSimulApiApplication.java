f08d6c019b1856ed520c66bf89250ef111b80284
Łukasz Królicki <lukasmatrix12345@gmail.com>
2024-07-30 14:51:37 +0200

    First commit

---
+++ src/main/java/com/rest/diseasesimulapi/DiseaseSimulApiApplication.java
@@ -0,0 +1,13 @@
+package com.rest.diseasesimulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiseaseSimulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiseaseSimulApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rest/diseasesimulapi/DiseaseSimulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.diseasesimulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiseaseSimulApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
